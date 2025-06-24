c489923d0b7e2ed7f301833660221b622ec4c525
PTHARRISH <harrishpt@gmail.com>
2025-06-24 11:50:14 +0530

    admin export import

--- LittleLemonAPI/admin.py
+++ LittleLemonAPI/admin.py
@@ -1,7 +1,38 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
+from import_export import resources
 from import_export.admin import ImportExportModelAdmin
+from rest_framework.authtoken.models import Token
 
 from .models import Cart, Category, MenuItem, Order, OrderItem
+
+# --- Export support for built-in models ---
+admin.site.unregister(User)
+admin.site.unregister(Group)
+
+class UserResource(resources.ModelResource):
+    class Meta:
+        model = User
+
+class GroupResource(resources.ModelResource):
+    class Meta:
+        model = Group
+
+class TokenResource(resources.ModelResource):
+    class Meta:
+        model = Token
+
+@admin.register(User)
+class CustomUserAdmin(ImportExportModelAdmin):
+    resource_class = UserResource
+
+@admin.register(Group)
+class CustomGroupAdmin(ImportExportModelAdmin):
+    resource_class = GroupResource
+
+@admin.register(Token)
+class TokenAdmin(ImportExportModelAdmin):
+    resource_class = TokenResource
 
 
 @admin.register(MenuItem)
